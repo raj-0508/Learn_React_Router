dd8f2ed7d42f0a42f8f7227a447b74bcbe4056ab
Raj Singhaniya <rajsinghaniya0508@gmail.com>
2025-06-04 21:20:52 +0530

    Fix route path for Home component to use root path

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path="home" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="contact" element={<Contact />} />
           <Route path="github" element={<Github />} />
